61ff59041a4b01e25fc01beaa1968ef792cd4dfe
skilloutofboundsexception <mecayounbloque@hotmail.com>
2017-08-10 15:51:52 -0600

    read/wrote

--- main.cpp
+++ main.cpp
@@ -11,15 +11,6 @@ using namespace std;
 
 int main(){
 
-    Bloque * b = new Bloque(0);
-    BloqueTabla * bt = new BloqueTabla(1);
-
-    b->read();
-    bt->read();
-
-    bt->tablas->index(0)->printTabla();
-
-
     return 0;
 }
 
